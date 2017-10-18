9c27efd45c34637d8d51c6c866fa1b2ad2a4f4c4
Jeremy Jones <Jayleejones13@gmail.com>
2017-10-18 17:43:27 -0600

    Wednesday-practice

---
+++ exercises/loop-olympics.js
@@ -0,0 +1,47 @@
+// for (var i = 0; i <= 9; i++)
+// console.log(i);
+
+// for (var i = 0; i <= 9; i++)
+// console.log(i);
+
+// var array = ['Banana', "Orange", "Apple", "Kiwi"];
+// for (var i = 0; i < array.length; i++){
+  // console.log(array[i]);
+// }
+
+
+// for (var i = 0; i <= 9; i++)
+// console.log(i);
+
+var peopleArray = [
+  {
+    name: "Harrison Ford",
+    occupation: "Actor"
+  },
+  {
+    name: "Justin Beiber",
+    occupation: "Singer"
+  },
+  {
+    name: "Vladmir Putin",
+    occupation: "Politition"
+  },
+  {
+    name: "Oprah",
+    occupation: "Entertainer"
+  }
+]
+var myArray = [];
+// console.log(peopleArray[0].name)
+
+for (var i = 0; i < peopleArray.length; i+=2){
+  myArray.push(peopleArray[i].name)
+}
+console.log(myArray);
+
+//gold medal for loop excericse 
+for(var i = 0; i < 10; i++){
+  for (var j = 0; j < 10; j++){
+
+  }
+}
---
+++ exercises/statement-olympics.js
@@ -0,0 +1,53 @@
+//if (5 > 3);
+//console.log('Is greater than');
+
+//var cat = 'cat';
+//if('Cat' === 3);
+//console.log(cat.length);
+
+
+/*if ('cat' == 'dog');{
+  console.log(true)
+}else{
+ console.log('not the same');
+}
+  console.log('not the same');
+};*/
+
+
+
+/*var person = {
+}
+if ( person.age < 18){
+  console.log('Can go to movies');
+}else{
+  console.log('Busted!');
+}*/
+
+/*var person = {
+  name: "Bobby",
+  age: 12
+}
+if (person.name[0]=== 'B'){
+  console.log('Allowed');
+}*/
+
+
+/*var person = {
+  name: "Bobby",
+  age: 12
+}
+if (person.age >= 18 && person.name[0] === 'B'){
+  console.log('Allowed');
+}else{
+  console.log('Busted');
+}*/
+/*if (1 <= 2)
+console.log('Yes');{
+  else
+}*/
+
+
+
+var dog = dog;
+dog === dog ? console.log('Good boy') : console.log('Pound')

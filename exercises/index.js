0333bc07aa7a9a1fa39ea6d9e56fbcb0cc007356
Jeremy Jones <jleepc@Jeremys-MacBook-Pro.local>
2017-10-18 17:43:27 -0600

    Wednesday-practice

---
+++ exercises/index.js
@@ -0,0 +1,34 @@
+if (bagels are in the fridge){
+  eatBreakfast()
+}else{
+
+};
+
+logic operators
+false && false //false
+false && true // false
+true && false //false
+true && true //true
+
+true || true // true
+true || false // true
+true ||
+// if its within +-10 of 100 or 200 return true else return false
+
+
+if (age < 5){
+  console.log('too young');
+}else{
+  console.log('old enough')
+}
+
+ternary example
+age < 5 ? console.log('too young') : console.log('old enough');
+
+  var greeting;
+  if (name.length > 6){
+    greeting = 'Hello';
+  }else{
+    greeting = 'Goodbye'
+  }
+console.log(greeting);
---
+++ exercises/objectsInTheRoom.js
@@ -0,0 +1,18 @@
+var monitor = {
+  type:"Asus",
+  model:500,
+  works: true
+};
+
+var chair = {
+  type:["Office","Swivel", 5],
+  model:"9000",
+  color:"Black"
+};
+
+var classMate = {
+  type:"Human",
+  model:"Male",
+  age:35,
+  eyeColor:"Brown"
+};

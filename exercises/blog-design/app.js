49f537c557c0a909c21f4954f3e6546cfe66e8ee
Jeremy Jones <jleepc@Jeremys-MBP.domain>
2017-10-22 17:45:31 -0600

    new-css-files

---
+++ exercises/blog-design/app.js
@@ -0,0 +1,17 @@
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+    if (document.body.scrollTop > 450 || document.documentElement.scrollTop > 450) {
+        document.getElementById("myBtn").style.display = "block";
+    } else {
+        document.getElementById("myBtn").style.display = "none";
+    }
+}
+
+
+function topFunction() {
+    document.body.scrollTop = 0;  
+    document.documentElement.scrollTop = 0; 
+}
+console.log(topFunction);

2bbaccbacf1e6a0f8319f2f1e9ac098a010af292
Jeremy Jones <jleepc@Jeremys-MacBook-Pro.local>
2017-10-24 09:04:05 -0600

    working files

--- exercises/blog-design/app.js
+++ exercises/blog-design/app.js
@@ -14,4 +14,4 @@ function topFunction() {
     document.body.scrollTop = 0;  
     document.documentElement.scrollTop = 0; 
 }
-console.log(topFunction);
+console.log();
---
+++ exercises/strunctions/app.js
@@ -0,0 +1,54 @@
+// var str1 = 'Hello there!';
+// var str2 = "Goodbye";
+// var returned = str1 + str2
+// str1.concat(str2 + " , ");
+// console.log(returned);
+
+// var str1 = "12381493912832198";
+// var returned = str1.indexOf(4);
+// console.log(returned);
+// console.log(str1);
+
+// var str1 = "123814939128372198";
+// var returned = str1.lastIndexOf(7);
+// console.log(returned);
+// console.log(str1);
+
+// var str = 'SEE the movies';
+// var re = 'See thEE show';
+// var found = str.match(/ee/gi);
+//
+// console.log(found);
+
+// var str1 = 'superduper';
+// var returned = str1.toUpperCase();
+// str1.toUpperCase();
+// console.log(str1);
+// console.log(returned);
+
+// var str2 = 'SUPERDUPER';
+// var returned = str2.toLowerCase();
+// str2.toLowerCase();
+// console.log(str2);
+// console.log(returned);
+
+
+// var str2 = 'SUPERDUPER';
+// var returned = str2.slice(0,5);
+// console.log(str2);
+// console.log(returned);
+
+
+// var string = 'Who didnt take the trash out';
+// var split = string.split(" ,", 3);
+//
+// console.log(split);
+
+// var str = "Go to your moms house";
+// var str2 = str.replace("your moms house", "vschool");
+// console.log(str2);
+
+var str = '1234671234567';
+var returned = str.substr(-3);
+console.log(returned);
+console.log(str);

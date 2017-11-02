4f9ff667ba547221584d5416010581a844199d75
Jeremy Jones <Jayleejones13@gmail.com>
2017-11-01 20:52:57 -0600

    added and worked files

---
+++ exercises/testing/app.js
@@ -0,0 +1,19 @@
+// var add = function(a,b){
+//   return a + b === 4;
+// }
+// module.exports = add;
+
+
+//pallindrome function
+// var palindrome = function(str){
+//   var arrStr = str.split(" ");
+//     var newArr = arrStr.reverse();
+//   var reverseStr = newArr.join(" ");
+//
+//   if (reverseStr === str){
+//     return true
+//   }else{
+//     return false
+//   }
+// }
+// module.exports = palindrome;
